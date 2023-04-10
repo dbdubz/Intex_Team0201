e7069118a45a4e42fce48f2c2cac3ad007b137aa
Dalyn Baldwin <101284161+dbdubz@users.noreply.github.com>
2023-04-10 09:11:45 -0600

    Added DB

--- backend/obj/Debug/netcoreapp3.1/backend.AssemblyInfo.cs
+++ backend/obj/Debug/netcoreapp3.1/backend.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap4")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-backend-0896C262-D9AB-4A74-A038-EE5EA1E40EFA")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
